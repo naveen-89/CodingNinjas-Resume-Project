108ec6bf8d026572c293686e426cffe57a74ab3e
Naveen Gupta <naveen1568.cse19@chitkara.edu.in>
2021-08-30 23:52:28 +0530

    First Upload of Project

---
+++ responsive.css
@@ -0,0 +1,154 @@
+@media only screen and (max-width: 572px) {
+  body {
+    font-size: 12px;
+  }
+}
+
+/* Responsive Navbar*/
+@media only screen and (max-width: 768px) {
+  .nav-menu li {
+    margin: 0px 4px;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  #body-header {
+    font-size: 12px;
+    height: 35vh;
+    min-height: 350px;
+  }
+  #body-header #name-social-container {
+    padding-top: 6em;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  #about #self-info {
+    width: 80%;
+    min-width: 0;
+  }
+  #about #profile-image {
+    min-width: 180px;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .contact-container {
+    min-width: 0;
+  }
+  .contact-container .contact-form-container {
+    margin-bottom: 5rem;
+  }
+  .contact-container .contact-form-container, .contact-container .address-container {
+    flex-basis: 80%;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .timeline {
+    width: 100%;
+    min-width: 0;
+  }
+  .timeline-divider {
+    height: 85%;
+    left: 5%;
+  }
+  .timeline-box {
+    margin: 2em;
+    width: 85%;
+  }
+  .timeline-box:nth-child(2n) {
+    left: 5%;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  #portfolio-container {
+    min-width: 0;
+    justify-content: center;
+  }
+  .portfolio-image-container {
+    width: 45%;
+  }
+}
+
+@media only screen and (max-width: 572px) {
+  #body-header {
+    font-size: 10px;
+    height: 45vh;
+    min-height: 280px;
+  }
+  #body-header .checkbtn {
+    display: block;
+  }
+  #body-header .nav-menu {
+    position: absolute;
+    min-width: 140px;
+    width: 20%;
+    height: auto;
+    background-color: rgba(150, 150, 150, 0.918);
+    top: 2.5em;
+    right: 5%;
+    box-shadow: 1px 1px 10px 4px white;
+    border-radius: 4px;
+    z-index: 100;
+    transition: all 0.3s;
+    opacity: 0;
+  }
+  #body-header .nav-menu li {
+    display: block;
+    padding: 0.2em;
+    padding-left: 16px;
+    line-height: 2.2em;
+    margin: 0;
+    text-align: left;
+  }
+  #body-header #check:checked ~ .nav-menu {
+    opacity: 1;
+  }
+  #name-social-container {
+    font-size: 10px;
+  }
+  #name-social-container #header-name {
+    letter-spacing: 0;
+  }
+  #name-social-container #header-name h1 {
+    margin: 0;
+  }
+}
+
+@media only screen and (max-width: 572px) {
+  .timeline {
+    font-size: 14px;
+    padding: 0 1rem;
+  }
+}
+
+@media only screen and (max-width: 572px) {
+  #contact .social-links {
+    font-size: 0.75em;
+  }
+  #contact .social-links i {
+    padding: 0 0.25em;
+  }
+}
+
+@media only screen and (max-width: 572px) {
+  .skill-progress {
+    width: 70%;
+  }
+}
+
+@media only screen and (max-width: 572px) {
+  #about #profile-image {
+    width: 25%;
+    min-width: 140px;
+  }
+}
+
+@media only screen and (max-width: 572px) and (min-height: 730px) {
+  #body-header {
+    height: 35vh;
+  }
+}
+/*# sourceMappingURL=responsive.css.map */
